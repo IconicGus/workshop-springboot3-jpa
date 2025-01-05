dfdce3bc473a032ef5e0495776a45b263902e930
Gustavo Marcal <eletronica1507@gmail.com>
2025-01-05 14:42:51 -0300

    Payment, one-to-one association

--- src/main/java/com/iconicgus/springboot/entities/Order.java
+++ src/main/java/com/iconicgus/springboot/entities/Order.java
@@ -27,12 +27,15 @@ public class Order implements Serializable {
 
     private Integer orderStatus;
 
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
+
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
     public Order(){
 
     }
-
-    @OneToMany(mappedBy = "id.order")
-    private Set<OrderItem> items = new HashSet<>();
 
     public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
@@ -77,6 +80,14 @@ public class Order implements Serializable {
 
     public Set<OrderItem> getItems() {
         return items;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     @Override
